45dbd356b9b12b86d9e08efce21866c5c2140481
Nikhil <nikhil.ahirwar-ext@hpe.com>
2026-09-04 18:27:17 +0530

    fix: allow subadmins to view reviews and review stats in admin portal

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -17,8 +17,8 @@ router.get("/my", protect, getMyReview);
 router.patch("/my", protect, updateReview);
 
 // Admin routes
-router.get("/all", protect, restrictTo("admin"), getAllReviews);
-router.get("/stats", protect, restrictTo("admin"), getReviewStats);
+router.get("/all", protect, restrictTo("admin", "subadmin"), getAllReviews);
+router.get("/stats", protect, restrictTo("admin", "subadmin"), getReviewStats);
 router.delete("/:id", protect, restrictTo("admin"), deleteReview);
 
 module.exports = router;
